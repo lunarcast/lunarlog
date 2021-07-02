a3a71d90da532737d52f1b92f19b71e4aa6ae610
Matei Adriel <rafaeladriel11@gmail.com>
2021-07-02 18:17:35 +0300

    feat(client): flex layout demo

--- packages/client/src/Main.purs
+++ packages/client/src/Main.purs
@@ -3,13 +3,13 @@ module Main where
 import Loglude
 
 import Data.Array.NonEmpty as NonEmptyArray
-import Data.Typelevel.Undefined (undefined)
-import Debug (spy)
-import Geometry (Context2D, Geometry, Tea, bounds, launchTea)
+import Effect.Class.Console (log)
+import Geometry (Context2D, Geometry, Tea, launchTea, x, y)
 import Geometry as Geometry
-import Geometry.Shapes.Flex (FlexAxis(..), createLayout, fixedSizeLayout)
+import Geometry.Shapes.Flex (Alignment(..), Arrangement(..), createLayout, fixedSizeLayout, withMinimumSize)
 import Geometry.Shapes.Padding as Padding
-import Graphics.Canvas (getCanvasElementById, getContext2D, scale)
+import Geometry.Vector (Axis(..))
+import Graphics.Canvas (getCanvasElementById, getContext2D)
 import Loglude.Cancelable as Cancelable
 import Lunarlog.Canvas (fixDpi)
 import Lunarlog.Client.VisualGraph.Types as VisualGraph
@@ -30,47 +30,78 @@ myPattern =
 myVisualPattern :: VisualGraph.Pattern
 myVisualPattern = { position: vec2 100.0 200.0, width: 100.0 }
 
-scene :: Context2D -> Tea Unit Unit
+data MyAction = Clicked String
+
+scene :: Context2D -> Tea Unit MyAction
 scene context = { context, initialState: unit, render, handleAction, setup }
     where
     setup = const $ pure unit
-    handleAction = const $ pure unit
+    handleAction = case _ of
+        Clicked msg -> log $ "Clicked!!! " <> msg 
+
+    arrangements = NonEmptyArray.cons' ArrangeStart [ArrangeCenter, ArrangeEnd, SpaceBetween, SpaceEvenly]
     
-    render :: Ask Context2D => Unit -> Geometry Unit
-    render _ = Geometry.group 
-        { children: 
-           [ Geometry.rect 
-                { position: vec2 100.0 200.0
-                , size: layout.minimumSize
-                , fill: "blue" 
-                }
-           , geom
-           ] 
-        }
-        where
-        bruh = spy "bounds" $ bounds geom
-        geom = spy "geometry" $ fixedSizeLayout layout layout.minimumSize
-        layout = spy "layout" $ createLayout
-            { children: bgify <$> NonEmptyArray.cons' a [b, c] 
-            , position: vec2 100.0 200.0
-            , flexAxis: FlexY
-            , stretchChildren: false
-            , arrangeChildren: undefined
-            , alignChildren: undefined
+    render :: Ask Context2D => Unit -> Geometry _
+    render _ = withMinimumSize $ createLayout
+            { children: NonEmptyArray.mapWithIndex renderArrangement arrangements
+            , position: zero
+            , flexAxis: X
+            , stretchChildren: true
+            , arrangeChildren: ArrangeStart
+            , alignChildren: AlignStart
             }
 
-        bgify g = Geometry.aabbPadding 
+    renderArrangement :: Ask Context2D => Int -> Arrangement -> Geometry _
+    renderArrangement index arrangement = withMinimumSize $ createLayout
+        { children: flip NonEmptyArray.cons' 
+           [ Geometry.group 
+                { children:  
+                    [ Geometry.rect 
+                        { position: vec2 0.0 40.0
+                        , size 
+                        , fill: "blue" 
+                        }
+                    , geom
+                    ]
+                }
+           ] $ Geometry.aabbPadding  
+                { target: Geometry.text 
+                    { text: show arrangement
+                    , font: "20px Source Code Pro"
+                    , position: zero
+                    , fill: "black"
+                    }
+                , amount: Geometry.equalPadding 10.0
+                }
+        , position: zero
+        , flexAxis: Y
+        , stretchChildren: true
+        , arrangeChildren: ArrangeStart
+        , alignChildren: AlignMiddle
+        }
+        where
+        geom = fixedSizeLayout layout size 
+        size = vec2 (x layout.minimumSize) (y layout.minimumSize + 100.0)
+        layout = createLayout
+            { children: addPadding <$> NonEmptyArray.cons' a [b, c] 
+            , position: vec2 0.0 40.0
+            , flexAxis: Y
+            , stretchChildren: false
+            , arrangeChildren: arrangement
+            , alignChildren: AlignMiddle
+            }
+
+        addPadding g = Geometry.aabbPadding 
                     { target: g
                     , amount: Geometry.equalPadding 10.0 
                     , fill: "yellow"
+                    , onClick: const $ Clicked "something else"
                     , paddingPlacement: Padding.FixedCorner
                     }
 
-        a = Geometry.text { text: "Adriel", font: "30px Arial", position: zero, stroke: "black" } 
+        a = Geometry.text { text: "Something", onClick: const $ Clicked "something", font: "30px Arial", position: zero, stroke: "black" } 
         b = Geometry.text { text: "Short", position: zero, fill: "black" } 
         c = Geometry.text { text: "Long name!", font: "20px Source Code Pro", position: zero, fill: "black" } 
-        -- spy "geometry" $ 
-        -- renderPattern myVisualPattern myPattern
 
 main :: Effect Unit
 main = do
@@ -79,6 +110,6 @@ main = do
         fixDpi ctx
 
         let zoom = 2.0
-        scale ctx { scaleX: zoom, scaleY: zoom }
+        -- scale ctx { scaleX: zoom, scaleY: zoom }
 
         Cancelable.perform $ launchTea $ scene ctx
