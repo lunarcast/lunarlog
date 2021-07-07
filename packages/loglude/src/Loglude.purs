6c7e1680bfb1563afcc016b8951b0ca715cbd9cb
Matei Adriel <rafaeladriel11@gmail.com>
2021-07-08 01:45:46 +0300

    feat(loglude): Even more reexports...

--- packages/loglude/src/Loglude.purs
+++ packages/loglude/src/Loglude.purs
@@ -38,21 +38,34 @@ module Loglude
     , module Data.Lens.Lens.Tuple
     , module Data.Lens.Iso.Newtype
     , module Data.Newtype
+    , module Type.Row
+    , module Data.ZipperArray
+    , module Run
+    , module Run.Except
+    , module Run.Reader
+    , module Run.State
+    , module Run.Writer
     , module Data.Tuple.Nested ) where
 
 import Prelude
 
-import Data.Newtype (class Newtype, unwrap, wrap)
-import Data.Lens.Lens.Tuple (_1, _2)
+import Run (EFFECT, Run, AFF)
+import Run.Except (EXCEPT, FAIL)
+import Run.Reader (READER)
+import Run.State (STATE)
+import Run.Writer (WRITER)
+
 import Data.Array.NonEmpty (NonEmptyArray)
-import Data.Foldable (class Foldable, foldr, foldl, foldMap, sum)
+import Data.Foldable (class Foldable, foldMap, foldl, foldr, for_, sum)
 import Data.Generic.Rep (class Generic)
 import Data.HashMap (HashMap)
-import Data.Int (floor, toNumber)
+import Data.Int (floor, toNumber, even, odd)
 import Data.Lens (Lens', Lens, Setter, Setter', Getter, Getter', over, set, preview, view, lens)
 import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Lens.Lens.Tuple (_1, _2)
 import Data.Lens.Record (prop)
 import Data.Maybe (Maybe(..), fromMaybe, maybe, maybe')
+import Data.Newtype (class Newtype, unwrap, wrap)
 import Data.Show.Generic (genericShow)
 import Data.Symbol (class IsSymbol)
 import Data.Traversable (class Traversable, traverse_, traverse)
@@ -61,6 +74,7 @@ import Data.Tuple.Nested (type (/\), (/\))
 import Data.Typelevel.Num (D0, D1, D2, D3, D4, D6, d0, d1, d2, d3, d4, d6)
 import Data.Undefined.NoProblem (Opt, fromOpt, opt)
 import Data.Vec (Vec, vec2)
+import Data.ZipperArray (ZipperArray)
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Foreign (Foreign)
@@ -74,6 +88,7 @@ import Loglude.Types (Id, Const, Pair)
 import Prim.Row (class Union)
 import Prim.RowList (class RowToList, RowList)
 import Type.Proxy (Proxy(..))
+import Type.Row (type (+))
 import Type.RowList (class ListToRow)
 import Unsafe.Coerce (unsafeCoerce)
 import Web.Event.EventTarget (eventListener)
