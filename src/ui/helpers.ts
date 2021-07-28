4bd72b07977b93d1aa737be69e20748a38a93af4
Matei Adriel <rafaeladriel11@gmail.com>
2021-07-28 19:28:46 +0300

    feat(ui): rule panel (idr tbh)

--- src/ui/helpers.ts
+++ src/ui/helpers.ts
@@ -1,5 +1,5 @@
+export const capitalizeWord = (word: string): string =>
+  word[0].toUpperCase() + word.slice(1);
+
 export const capitalize = (s: string): string =>
-  s
-    .split(" ")
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(" ");
+  s.split(" ").map(capitalizeWord).join(" ");
--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -3,10 +3,20 @@ import { useState, useEffect } from "preact/hooks";
 import { Button, Input } from "./Input";
 import { ComponentChild } from "preact";
 import { Spacing } from "./Spacing";
-import { capitalize } from "./helpers";
+import { capitalize, capitalizeWord } from "./helpers";
+import { div } from "@thi.ng/vectors";
 
 // ========== Types
+interface RuleBranch {
+  thumbail: string;
+}
+
+interface Rule {
+  branches: Array<RuleBranch>;
+}
+
 type NodeMemory = Record<string, number>;
+type RuleMemory = Record<string, Rule>;
 
 interface EditorPaneProps {
   title: string;
@@ -22,6 +32,21 @@ interface NodeListProps {
 interface NodeProps {
   name: string;
   argumentCount: number;
+}
+
+interface RuleListProps {
+  rules: RuleMemory;
+  disabled?: boolean;
+}
+
+interface RuleBranchProps {
+  thumbail: string;
+  name: string;
+}
+
+interface BranchListProps {
+  branches: Array<RuleBranch>;
+  name: string;
 }
 
 interface CreateCompoundProps {
@@ -84,6 +109,7 @@ const EmptyNodeList = () => {
   );
 };
 
+// The node list panel
 const NodeList = ({ nodes, disabled }: NodeListProps) => {
   return (
     <EditorPane
@@ -102,6 +128,48 @@ const NodeList = ({ nodes, disabled }: NodeListProps) => {
   );
 };
 
+// The preview for a rule
+const RuleBranch = ({ thumbail, name }: RuleBranchProps) => {
+  return (
+    <div className="rule__branch">
+      <img
+        src={thumbail}
+        height={100}
+        alt={name}
+        className="rule__branch-thumbail"
+      />
+    </div>
+  );
+};
+
+// The rule list panel
+const BranchList = ({ branches, name }: BranchListProps) => {
+  return (
+    <div className="rule">
+      <div className="rule__name">{capitalizeWord(name)}</div>
+      <div className="rule__branches">
+        {branches.map(({ thumbail }, index) => (
+          <RuleBranch name={name} thumbail={thumbail} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const RuleList = ({ rules, disabled }: RuleListProps) => {
+  return (
+    <EditorPane
+      title="Rules"
+      disabled={disabled ? "No rules found" : undefined}
+    >
+      {Object.entries(rules).map(([name, branches]) => (
+        <BranchList name={name} key={name} branches={branches.branches} />
+      ))}
+    </EditorPane>
+  );
+};
+
+// Used for the create node and create rule components
 const CreateCompound = ({
   isTaken,
   createNode,
@@ -175,9 +243,31 @@ const CreateCompound = ({
 
 export const EditorUi = () => {
   const [nodeMemory, setNodeMemory] = useState<NodeMemory>({});
+  const [rules, setRules] = useState<RuleMemory>({
+    myRule: {
+      branches: [
+        {
+          thumbail:
+            "https://images-na.ssl-images-amazon.com/images/I/41g6jROgo0L.png",
+        },
+        {
+          thumbail:
+            "https://images-na.ssl-images-amazon.com/images/I/41g6jROgo0L.png",
+        },
+      ],
+    },
+  });
 
   return (
     <div id="editor">
+      <CreateCompound
+        productName={"rule"}
+        isTaken={(name) => Reflect.has(nodeMemory, name)}
+        createNode={(name, argumentCount) =>
+          setRules((old) => ({ ...old, [name]: { branches: [] } }))
+        }
+      />
+      <RuleList disabled={Object.entries(rules).length === 0} rules={rules} />
       <CreateCompound
         disabled
         allowUpdates
@@ -188,14 +278,6 @@ export const EditorUi = () => {
         }
       />
       <NodeList disabled nodes={[...Object.entries(nodeMemory)]} />
-      <CreateCompound
-        productName={"rule"}
-        isTaken={(name) => Reflect.has(nodeMemory, name)}
-        createNode={(name, argumentCount) =>
-          setNodeMemory((old) => ({ ...old, [name]: argumentCount }))
-        }
-      />
-      <div className="editor__pane">3</div>
       <div className="editor__pane">7</div>
       <div className="editor__pane">8</div>
     </div>
