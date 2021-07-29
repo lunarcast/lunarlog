0e7eb4b23115c9e61086dee833c09505ddfe0154
Matei Adriel <rafaeladriel11@gmail.com>
2021-07-29 14:16:42 +0300

    feat(loglude): `catchErrors` helper

---
+++ packages/loglude/src/Catch.purs
@@ -0,0 +1,9 @@
+module Loglude.Catch where
+
+import Data.Either (Either(..))
+
+foreign import catchErrorsImpl :: forall a b e r. (e -> r) -> (b -> r) -> (a -> b) -> (a -> r)
+
+-- | Surround a function with a try catch block. Useful for ffi stuff.
+catchErrors :: forall a b e. (a -> b) -> a -> Either e b
+catchErrors = catchErrorsImpl Left Right
